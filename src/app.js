02b29153712815d8ab69e20f686f3cf56c59468e
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-02-03 13:10:16 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Hello} from "./hello"
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
